Added possibility to skip atoms in symm_unique fixed calculation of quality indicators fixed symm_equiv

gpaw_source.py
@@ -220,12 +220,19 @@ def calc_f0j(cell_mat_m, element_symbols, positions, index_vec_h, symm_mats_vecs
         del(gpaw_dict['average_symmequiv'])
     else:
         average_symmequiv = False
+    if 'skip_symm' in gpaw_dict:
+        assert len(gpaw_dict['skip_symm']) == 0 or average_symmequiv, 'skip_symm does need average_symmequiv' 
+        skip_symm = gpaw_dict['skip_symm']
+        del(gpaw_dict['skip_symm'])
+    else:
+        skip_symm = {}
 
     #assert not (not average_symmequiv and not do_not_move)
     symm_positions, symm_symbols, f0j_indexes = expand_symm_unique(element_symbols,
                                                                    np.array(positions),
                                                                    np.array(cell_mat_m),
-                                                                   symm_mats_vecs)
+                                                                   (np.array(symm_mats_vecs[0]), np.array(symm_mats_vecs[1])),
+                                                                   skip_symm=skip_symm)
     if restart is None:
         atoms = crystal(symbols=symm_symbols,
                         basis=symm_positions % 1,
@@ -278,25 +285,25 @@ def calc_f0j(cell_mat_m, element_symbols, positions, index_vec_h, symm_mats_vecs
 
     if average_symmequiv:
         h, k, l = np.meshgrid(*map(lambda n: np.fft.fftfreq(n, 1/n).astype(np.int64), density.shape), indexing='ij')
-        for atom_index, symm_atom_indexes in enumerate(f0j_indexes.T):
+        for atom_index, symm_atom_indexes in enumerate(f0j_indexes):
             f0j_sum = np.zeros_like(h, dtype=np.complex128)
             for symm_matrix, symm_atom_index in zip(symm_mats_vecs[0], symm_atom_indexes):
                 h_density = density * partitioning.hdensity.get_density([symm_atom_index], gridrefinement=gridrefinement, skip_core=explicit_core)[0] / overall_hdensity
                 frac_position = symm_positions[symm_atom_index]
-                h_rot, k_rot, l_rot = np.einsum('yx, y... -> x...', symm_matrix, np.array((h, k, l))).astype(np.int64)
+                h_rot, k_rot, l_rot = np.einsum('xy, y... -> x...', symm_matrix, np.array((h, k, l))).astype(np.int64)
                 phase_to_zero = np.exp(-2j * np.pi * (frac_position[0] * h + frac_position[1] * k + frac_position[2] * l))
                 f0j_sum += (np.fft.ifftn(h_density) * phase_to_zero * np.prod(h.shape))[h_rot, k_rot, l_rot]
             f0j_sum /= len(symm_atom_indexes)
 
             for symm_index, symm_matrix in enumerate(symm_mats_vecs[0]):
-                h_rot, k_rot, l_rot = np.einsum('xy, zy -> zx', symm_matrix, index_vec_h).astype(np.int64).T
+                h_rot, k_rot, l_rot = np.einsum('xy, zy -> zx', symm_matrix.T, index_vec_h).astype(np.int64).T
                 f0j[symm_index, atom_index, :] = f0j_sum[h_rot, k_rot, l_rot]
     else:
         #TODO Is a discrete Fourier Transform just of the hkl we need possibly faster? Can we then interpolate the density to get even better factors?
         # This could also save memory, fft is O(NlogN) naive dft is probably N^2
         h_vec, k_vec, l_vec = index_vec_h.T
         already_known = {}
-        for atom_index, symm_atom_indexes in enumerate(f0j_indexes.T):
+        for atom_index, symm_atom_indexes in enumerate(f0j_indexes):
             f0j_sum = np.zeros_like(density, dtype=np.complex128)
             for symm_index, (symm_matrix, symm_atom_index) in enumerate(zip(symm_mats_vecs[0], symm_atom_indexes)):
                 if symm_atom_index in list(already_known.keys()):

io.py
@@ -333,7 +333,7 @@ def lst2constraint_dict(filename):
 
 def write_fcf(filename, hkl, refine_dict, parameters, symm_strings, structure_factors, cell):
     cell_mat_m = cell_constants_to_M(*cell)
-    cell_mat_f = np.linalg.inv(cell_mat_m)
+    cell_mat_f = np.linalg.inv(cell_mat_m).T
     index_vec_h = hkl[['h', 'k', 'l']].values
     intensity = hkl['intensity'].values
     stderr = hkl['stderr'].values
@@ -346,7 +346,6 @@ def write_fcf(filename, hkl, refine_dict, parameters, symm_strings, structure_fa
         extinction_parameter = 1
 
     if refine_dict['extinction'] == 'none':
-        intensities_calc = parameters[0] * np.abs(structure_factors)**2
         f_calc = np.abs(structure_factors)
         intensity_fcf = intensity / parameters[0]
         stderr_fcf = stderr
@@ -354,7 +353,6 @@ def write_fcf(filename, hkl, refine_dict, parameters, symm_strings, structure_fa
         i_calc0 = np.abs(structure_factors)**2
         if refine_dict['extinction'] == 'secondary':
             # Secondary exctinction, as shelxl needs a wavelength                
-            intensities_calc = parameters[0] * i_calc0 / (1 + parameters[extinction_parameter] * i_calc0)
             f_calc = np.abs(structure_factors) * np.sqrt(parameters[0] / (1 + parameters[extinction_parameter] * i_calc0)) 
             intensity_fcf = intensity / parameters[0] * (1 + parameters[extinction_parameter] * i_calc0)
             stderr_fcf = stderr / parameters[0] * (1 + parameters[extinction_parameter] * i_calc0)
@@ -363,7 +361,6 @@ def write_fcf(filename, hkl, refine_dict, parameters, symm_strings, structure_fa
             sintheta = np.linalg.norm(np.einsum('xy, zy -> zx', cell_mat_f, index_vec_h), axis=1) / 2 * wavelength
             sintwotheta = 2 * sintheta * np.sqrt(1 - sintheta**2)
             extinction_factors = 0.001 * wavelength**3 / sintwotheta
-            intensities_calc = parameters[0] * i_calc0 / np.sqrt(1 + parameters[extinction_parameter] * extinction_factors * i_calc0)
             f_calc = np.abs(structure_factors) * np.sqrt(parameters[0] / np.sqrt(1 + parameters[extinction_parameter] * extinction_factors * i_calc0)) 
             intensity_fcf = intensity / parameters[0] * np.sqrt(1 + parameters[extinction_parameter] * extinction_factors * i_calc0)
             stderr_fcf = stderr / parameters[0] * np.sqrt(1 + parameters[extinction_parameter] * extinction_factors * i_calc0)
@@ -437,12 +434,13 @@ def entries2atom_string(entries):
 def write_res(out_res_name, in_res_name, atom_table, cell, cell_std, wavelength, parameters):
     with open(in_res_name) as fo:
         res_lines = fo.readlines()
+    atom_table = atom_table.copy()
 
     latt_line = [line.strip() for line in res_lines if line.upper().startswith('LATT ')][0]
     symm_lines = [line.strip() for line in res_lines if line.upper().startswith('SYMM ')]
     symm_string = '\n'.join(symm_lines)
     sfac_line = [line.strip() for line in res_lines if line.upper().startswith('SFAC ')][0]
-    sfac_elements = sfac_line.split()[1:]
+    sfac_elements = [element.capitalize() for element in sfac_line.split()[1:]]
     unit_entries = ' '.join(['99'] * len(sfac_elements))
     sfac_df = pd.DataFrame({
         'sfac_index': np.arange(len(sfac_elements)) + 1,

quality.py
@@ -3,7 +3,7 @@ from .conversion import calc_sin_theta_ov_lambda, cell_constants_to_M, calc_sin_
 import numpy as np
 
 def calculate_quality_indicators(construction_instructions, parameters, fjs, cell_mat_m, symm_mats_vecs, index_vec_h, intensities, stderr):
-    cell_mat_f = np.linalg.inv(cell_mat_m)
+    cell_mat_f = np.linalg.inv(cell_mat_m).T
     xyz, uij, cijk, dijkl, occupancies = construct_values(parameters, construction_instructions, cell_mat_m)
 
     structure_factors = calc_f(

xharpy.py
@@ -17,7 +17,7 @@ from .restraints import resolve_restraints
 from .conversion import ucif2ucart, cell_constants_to_M
 
 
-def expand_symm_unique(type_symbols, coordinates, cell_mat_m, symm_mats_vec):
+def expand_symm_unique(type_symbols, coordinates, cell_mat_m, symm_mats_vec, skip_symm={}):
     """Expand the type_symbols and coordinates for one complete unit cell
     Will return an atom coordinate on a special position only once 
     also returns the matrix inv_indexes with shape n_symm * n_at for
@@ -27,17 +27,21 @@ def expand_symm_unique(type_symbols, coordinates, cell_mat_m, symm_mats_vec):
     un_positions = np.zeros((0, 3))
     n_atoms = 0
     type_symbols_symm = []
-    inv_indexes = np.zeros((symm_mats_r.shape[0], coordinates.shape[0]), dtype=np.int64)
+    inv_indexes = []
     # Only check atom with itself
-    for index, (pos0, type_symbol) in enumerate(zip(pos_frac0, type_symbols)):
-        symm_positions = (np.einsum('kxy, y -> kx', symm_mats_r, pos0) + symm_vecs_t) % 1
+    for atom_index, (pos0, type_symbol) in enumerate(zip(pos_frac0, type_symbols)):
+        if atom_index in skip_symm:
+            use_indexes = [i for i in range(symm_mats_r.shape[0]) if i not in skip_symm[atom_index]]
+        else:
+            use_indexes = list(range(symm_mats_r.shape[0]))
+        symm_positions = (np.einsum('kxy, y -> kx', symm_mats_r[use_indexes, :, :], pos0) + symm_vecs_t[use_indexes, :]) % 1
         _, unique_indexes, inv_indexes_at = np.unique(np.round(np.einsum('xy, zy -> zx', cell_mat_m, symm_positions), 5),
                                                       axis=0,
                                                       return_index=True,
                                                       return_inverse=True)
         un_positions = np.concatenate((un_positions, symm_positions[unique_indexes]))
         type_symbols_symm += [type_symbol] * unique_indexes.shape[0]
-        inv_indexes[:,index] = inv_indexes_at + n_atoms 
+        inv_indexes.append(inv_indexes_at + n_atoms)
         n_atoms += unique_indexes.shape[0]
     return un_positions.copy(), type_symbols_symm, inv_indexes
 
@@ -273,7 +277,7 @@ def create_construction_instructions(atom_table, constraint_dict, sp2_add, torsi
             constraint = constraint_dict[atom['label']]['occ']
             occupancy = FixedParameter(value=constraint.added_value[0])
         else:
-            occupancy = FixedParameter(value=1.0)
+            occupancy = FixedParameter(value=atom['occupancy'])
 
         construction_instructions.append(AtomInstructions(
             name=atom['label'],
@@ -763,8 +767,8 @@ def har(cell_mat_m, symm_mats_vecs, hkl, construction_instructions, parameters, 
         #)
         print(f'  wR2: {np.sqrt(x.fun / np.sum(hkl["intensity"].values**2 / hkl["stderr"].values**2)):8.6f}, nit: {x.nit}, {x.message}')
         parameters = jnp.array(x.x) 
-        #if x.nit == 0:
-        #    break
+        if x.nit == 0:
+            break
         if x.fun < r_opt_density or refine < 10:
             r_opt_density = x.fun
             #parameters_min1 = jnp.array(x.x)
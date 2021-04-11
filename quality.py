Start using weights instead of stds fix GOF bugfix in weighting

quality.py
@@ -27,7 +27,7 @@ def calculate_quality_indicators(construction_instructions, parameters, fjs, cel
     r_f_strong = np.sum(np.abs(f_obs[i_over_2sigma] - np.sqrt(parameters[0]) * np.abs(structure_factors[i_over_2sigma]))) / np.sum(np.abs(f_obs[i_over_2sigma]))
     r_f2 = np.sum(np.abs(intensities - parameters[0] * np.abs(structure_factors)**2)) / np.sum(intensities)
     wr2 = np.sqrt(np.sum(1/stderr**2 * (intensities - parameters[0] *  np.abs(structure_factors)**2)**2) / np.sum(1/stderr**2 * intensities**2))
-    gof = np.sum(1/stderr**2 * (intensities - parameters[0] * np.abs(structure_factors)**2)**2) / (len(intensities) - len(parameters))
+    gof = np.sqrt(np.sum(1/stderr**2 * (intensities - parameters[0] * np.abs(structure_factors)**2)**2) / (len(intensities) - len(parameters)))
 
     return {
             'R(F)': r_f,

weighting.py
@@ -2,7 +2,7 @@ from scipy.optimize import minimize
 import numpy as np
 import jax
 import jax.numpy as jnp
-from conversion import calc_sin_theta_ov_lambda
+from .conversion import calc_sin_theta_ov_lambda
 
 def calc_weights(parameters, c_mode, intensity_pos, i_calc, stderr, sintheta_ov_lambda=None):
     a, b, c, d, e, f = parameters
@@ -28,7 +28,7 @@ def weight_difference(refined_parameters, non_refined_parameters, intensity, i_c
 
     weights = calc_weights(parameters, c_mode, intensity, i_calc, stderr, sintheta_ov_lambda)
     
-    gof = jnp.sum((weights * (intensity - i_calc)**2)**2) / (n_i - n_p)
+    gof = jnp.sqrt(jnp.sum((weights * (intensity - i_calc)**2)**2) / (n_i - n_p))
 
     return (gof - 1)**2
     

xharpy.py
@@ -398,7 +398,7 @@ def calc_lsq_factory(cell_mat_m,
                      symm_mats_vecs,
                      index_vec_h,
                      intensities_obs,
-                     stds_obs,
+                     weights,
                      construction_instructions,
                      fjs_core,
                      flack_parameter,
@@ -446,7 +446,6 @@ def calc_lsq_factory(cell_mat_m,
             else:
                 intensities_calc = parameters[0] * i_calc0 / jnp.sqrt(1 + parameters[extinction_parameter] * extinction_factors * i_calc0)
                 #restraint_addition = 1.0 / 0.1 * parameters[extinction_parameter]**2
-        weights = 1 / stds_obs**2
 
         if flack_parameter is not None:
             structure_factors2 = calc_f(
@@ -485,7 +484,8 @@ def calc_var_cor_mat(cell_mat_m,
                      symm_mats_vecs,
                      index_vec_h,
                      construction_instructions,
-                     intensities_obs, stds_obs,
+                     intensities_obs,
+                     weights,
                      parameters, fjs,
                      fjs_core,
                      flack_parameter,
@@ -554,11 +554,11 @@ def calc_var_cor_mat(cell_mat_m,
     grad_func = jax.jit(jax.grad(function))
 
     collect = jnp.zeros((len(parameters), len(parameters)))
-    for index, weight in enumerate(1 / stds_obs**2):
+    for index, weight in enumerate(weights):
         val = grad_func(parameters, jnp.array(fjs), index)[:, None]
         collect += weight * (val @ val.T)
 
-    lsq_func = calc_lsq_factory(cell_mat_m, symm_mats_vecs, index_vec_h, intensities_obs, stds_obs, construction_instructions, fjs_core, flack_parameter, core_parameter, extinction_parameter, wavelength)
+    lsq_func = calc_lsq_factory(cell_mat_m, symm_mats_vecs, index_vec_h, intensities_obs, weights, construction_instructions, fjs_core, flack_parameter, core_parameter, extinction_parameter, wavelength)
     chi_sq = lsq_func(parameters, jnp.array(fjs)) / (index_vec_h.shape[0] - len(parameters))
 
     return chi_sq * jnp.linalg.inv(collect)
@@ -628,6 +628,21 @@ UEquivConstraint = namedtuple('UEquivConstraint', [
     'multiplicator' # Multiplicator for UEquiv Constraint (Usually nonterminal: 1.2, terminal 1.5)
 ])
 
+TrigonalPositionConstraint = namedtuple('TrigonalPositionConstraint', [
+    'bound_atom_name', # name of bound atom
+    'plane_atom1_name', # first bonding partner of bound atom
+    'plane_atom2_name', # second bonding partner of bound atom
+    'distance' # interatomic distance
+])
+
+TorsionPositionConstraint = namedtuple('TorsionCalculated', [
+    'bound_atom_name',   # index of  atom the derived atom is bound_to
+    'angle_atom_name',   # index of atom spanning the given angle with bound atom
+    'torsion_atom_name', # index of atom giving the torsion angle
+    'distance',           # interatom distance
+    'angle',              # interatom angle
+    'torsion_angle'       # interatom torsion angle
+])
 
 def har(cell_mat_m, symm_mats_vecs, hkl, construction_instructions, parameters, f0j_source='gpaw', reload_step=1, options_dict={}, refinement_dict={}, restraints=[]):
     """
@@ -702,6 +717,9 @@ def har(cell_mat_m, symm_mats_vecs, hkl, construction_instructions, parameters, 
     else:
         max_distance_diff = 1e-6
 
+    if 'weights' not in hkl.columns:
+        hkl['weights'] = 1 / hkl['stderr']**2
+
     print('  calculating first atomic form factors')
     if reload_step == 0:
         restart = 'save.gpw'
@@ -726,7 +744,7 @@ def har(cell_mat_m, symm_mats_vecs, hkl, construction_instructions, parameters, 
                                 symm_mats_vecs,
                                 jnp.array(hkl[['h', 'k', 'l']]),
                                 jnp.array(hkl['intensity']),
-                                jnp.array(hkl['stderr']),
+                                jnp.array(hkl['weights']),
                                 construction_instructions,
                                 f0j_core,
                                 flack_parameter,
@@ -808,7 +826,7 @@ def har(cell_mat_m, symm_mats_vecs, hkl, construction_instructions, parameters, 
                                    index_vec_h,
                                    construction_instructions,
                                    jnp.array(hkl['intensity']),
-                                   jnp.array(hkl['stderr']),
+                                   jnp.array(hkl['weights']),
                                    parameters,
                                    fjs,
                                    f0j_core,